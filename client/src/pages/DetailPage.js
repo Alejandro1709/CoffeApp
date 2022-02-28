2babf57820f9c7f0841d2b4812825a7df4aebe64
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-02-28 10:51:27 -0500

    add detail page

--- client/src/pages/DetailPage.js
+++ client/src/pages/DetailPage.js
@@ -19,15 +19,49 @@ function DetailPage() {
   loading && <span>loading...</span>;
 
   return (
-    <div>
+    <div className='mt-8'>
       {error && <span>{error}</span>}
-      {coffee ? <CoffeCard coffee={coffee} /> : <span>No Coffee Found</span>}
+      {coffee ? (
+        <div className='flex flex-col md:flex-row md:gap-3 justify-center items-center'>
+          {/* Coffee Preview */}
+          <CoffeCard coffee={coffee} disabled />
+          {/* Coffee Form */}
+          <div className=''>
+            <h3 className='text-2xl font-bold'>☕️ {coffee.coffeeName}</h3>
+            <form>
+              <div className='m-3'>
+                <select className='border p-2 rounded-md'>
+                  <option>-- SELECT COFFEE SIZE --</option>
+                  {coffee.coffeePrices &&
+                    coffee.coffeePrices.map((price) => (
+                      <option key={price._id}>
+                        {price.type} - S/. {price.price}
+                      </option>
+                    ))}
+                </select>
+              </div>
+              <div className='m-3'>
+                <input
+                  className='border p-2 rounded-md w-full'
+                  type='number'
+                  placeholder='Quantity'
+                  min={1}
+                  defaultValue={1}
+                />
+              </div>
+              <div className='mb-3'>
+                <button className='bg-green-800 text-white p-2 w-full rounded-md hover:bg-green-900'>
+                  Add to cart
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      ) : (
+        <span>No Coffee Found</span>
+      )}
     </div>
   );
 }
 
 export default DetailPage;
-
-// TODO:
-// REDUX
-// DETAIL PAGE
