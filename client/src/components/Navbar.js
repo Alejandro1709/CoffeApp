050747973728f1013694f1d95fd57ecf2e597d6b
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-02-28 10:51:16 -0500

    add link to navbar logo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <div className='flex justify-between items-center bg-white p-4'>
-      <div className='text-xl font-bold'>Coffee</div>
+      <Link to='/' className='text-xl font-bold'>
+        Coffee
+      </Link>
       <nav role='navigation'>
         <ul className='flex gap-4'>
           <li>Cart</li>
